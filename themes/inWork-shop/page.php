e4d4e3707f2605c3636e7b2c718b1f80f1652d6f
EdwinCNL <edwincornelisse@me.com>
2021-05-27 18:41:33 +0200

    Added the Booknetic on the front page

--- themes/inWork-shop/page.php
+++ themes/inWork-shop/page.php
@@ -1,1 +1,18 @@
+<?php get_header(); ?>
 
+<?php //This is the Explanation section?>
+<section class="section-container">
+
+    <?php  if (have_posts()) : while(have_posts()) : the_post();?>
+        <?php the_content();?>
+    <?php endwhile; endif;  ?>
+
+
+</section>
+
+<?php //This is the Partners section?>
+<section class="section-container">
+
+</section>
+
+<?php get_footer(); ?>
