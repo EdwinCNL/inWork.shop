61d302e0498826eab040531913926c14d37c9e4a
EdwinCNL <edwincornelisse@me.com>
2021-05-30 14:01:16 +0200

    Hot-fix add logic to detect local and live footer-form

--- themes/inWork-shop/include/contact.php
+++ themes/inWork-shop/include/contact.php
@@ -1,5 +1,12 @@
 <div class="section-container__contact-container">
     <div class="contact-container">
-        <?php echo do_shortcode( '[contact-form-7 id="21" title="footer-contact-form"]'); ?>
+        <?php
+        $pageUrlTest = get_permalink();
+        if (strpos($pageUrlTest, 'local')){
+            echo do_shortcode( '[contact-form-7 id="21" title="footer-contact-form"]');
+        } else {
+            echo do_shortcode('[contact-form-7 id="8" title="footer-contact-form"]');
+        }
+        ?>
     </div>
 </div>
